c4887662467b5e93fbb962b03810dc7a41d62a6e
Will Caylor <willcaylor@gmail.com>
2019-04-03 13:00:31 -0400

    replaced script and style files with package references

--- ProductComparison/App_Start/BundleConfig.cs
+++ ProductComparison/App_Start/BundleConfig.cs
@@ -7,19 +7,18 @@ namespace ProductComparison
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/vue").Include(
-                        "~/Scripts/vendor/vue.min.js",
-                        "~/Scripts/vendor/vue-resource.min.js",
-                        "~/Scripts/vendor/vue-router.min.js"));
+                        "~/Scripts/vue.min.js",
+                        "~/Scripts/vue-resource.min.js",
+                        "~/Scripts/vue-router.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/vendor/jquery-3.3.1.min.js",
-                      "~/Scripts/vendor/bootstrap.min.js",
-                      "~/Scripts/vendor/bootstrap.bundle.min.js"));
+                      "~/Scripts/jquery-3.0.0.min.js",
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/bootstrap.bundle.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.min.css",
                       "~/Content/bootstrap-grid.min.css",
-                      "~/Content/bootstrap-reboot.min.css",
                       "~/Content/site.css"));
         }
     }
